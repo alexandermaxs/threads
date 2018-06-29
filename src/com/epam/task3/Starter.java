68ab344a74cbc108c4f7a86708bf21e761a6516f
alexandermaxs <38385660+alexandermaxs@users.noreply.github.com>
2018-06-29 22:17:19 +0300

    Add files via upload

--- src/com/epam/task3/Starter.java
+++ src/com/epam/task3/Starter.java
@@ -2,12 +2,9 @@ package com.epam.task3;
 
 import com.epam.task3.model.CallCenter;
 import com.epam.task3.model.Client;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.xml.DOMConfigurator;
 
 public class Starter {
     public static void main(String[] args){
-        new DOMConfigurator().doConfigure("log4j.xml", LogManager.getLoggerRepository());
         CallCenter.getInstance().initialize();
         generateCalls(15);
     }
